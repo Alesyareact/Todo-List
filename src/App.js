0dbfef7e9bc660bcca0ad24b8bb043e368871217
Alesya Livanova <truealesya@gmail.com>
2021-08-20 11:14:50 +0300

    file formatting

--- src/App.js
+++ src/App.js
@@ -1,49 +1,70 @@
-import { useState } from 'react';
-import './App.css' 
-import TodoColumn from './Todo/TodoColumn';
+import { useState } from "react";
+import "./App.css";
+import TodoColumn from "./Todo/TodoColumn";
 
 function App() {
- 
+  const [tasks, setTasks] = useState([]);
 
-  const [tasks, setTasks] = useState([])
-
-
-  function changeStatus(body, status){
-    const result = tasks.findIndex( task => task.body === body)
-    tasks[result].status = status
-    setTasks([...tasks])
-    console.log("end")
-    console.log("tasks---")
-    console.log(tasks)
-  } 
+  function changeStatus(body, status) {
+    const result = tasks.findIndex((task) => task.body === body);
+    tasks[result].status = status;
+    setTasks([...tasks]);
+  }
 
   const removeTask = (id) => {
-    setTasks([...tasks.filter((task) => task.id !== id)])
-  }
+    setTasks([...tasks.filter((task) => task.id !== id)]);
+  };
 
   const addTask = (userInput, columnStatus) => {
     if (userInput) {
       const item = {
         id: Math.random().toString(36).substr(2, 9),
         body: userInput,
-        status: columnStatus 
-      }
-      setTasks([...tasks, item])
+        status: columnStatus,
+      };
+      setTasks([...tasks, item]);
     }
-  }
+  };
 
   return (
-    <div className='wrapper'>
-      <div className='card'> 
+    <div className="wrapper">
+      <div className="card">
         <h1>Todo List with React</h1>
-        <div className='lists'>
-          <TodoColumn columnName="Doing" columnStatus="doing" tasks={tasks.filter((task) => { return task.status === "doing" })} changeStatus = {changeStatus} remove={removeTask} addTask={addTask}/>
-          <TodoColumn columnName="Completed"columnStatus="completed" tasks={tasks.filter((task) => { return task.status === "completed"})} changeStatus = {changeStatus} remove={removeTask} addTask={addTask} />
-          <TodoColumn columnName="Rejected" columnStatus="rejected" tasks={tasks.filter((task) => { return task.status === "rejected" })} changeStatus = {changeStatus} remove={removeTask} addTask={addTask}/>
+        <div className="lists">
+          <TodoColumn
+            columnName="Doing"
+            columnStatus="doing"
+            tasks={tasks.filter((task) => {
+              return task.status === "doing";
+            })}
+            changeStatus={changeStatus}
+            remove={removeTask}
+            addTask={addTask}
+          />
+          <TodoColumn
+            columnName="Completed"
+            columnStatus="completed"
+            tasks={tasks.filter((task) => {
+              return task.status === "completed";
+            })}
+            changeStatus={changeStatus}
+            remove={removeTask}
+            addTask={addTask}
+          />
+          <TodoColumn
+            columnName="Rejected"
+            columnStatus="rejected"
+            tasks={tasks.filter((task) => {
+              return task.status === "rejected";
+            })}
+            changeStatus={changeStatus}
+            remove={removeTask}
+            addTask={addTask}
+          />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
--- src/Todo/TodoForm.js
+++ src/Todo/TodoForm.js
@@ -1,32 +1,37 @@
-import './TodoForm.css'
+import "./TodoForm.css";
 import { useState } from "react";
 
-function TodoForm({ addTask, columnStatus}) {
-    const [userInput, setUserInput] = useState('')
+function TodoForm({ addTask, columnStatus }) {
+  const [userInput, setUserInput] = useState("");
 
-    const handleChange = (e) => {
-        setUserInput(e.currentTarget.value)
+  const handleChange = (e) => {
+    setUserInput(e.currentTarget.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTask(userInput, columnStatus);
+    setUserInput("");
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleSubmit(e);
     }
-    
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        addTask(userInput, columnStatus)
-        setUserInput("")
-    }
+  };
 
-    const handleKeyPress = (e) => {
-        if(e.key === "Enter") {
-            handleSubmit(e)
-        }
-    }
-
-
-    return (
-        <form onClick={handleSubmit}>
-            <input type="text" size="40" value={userInput} onChange={handleChange} onKeyDown={handleKeyPress}/>
-            <button>Add</button>
-        </form>
-    )
+  return (
+    <form onClick={handleSubmit}>
+      <input
+        type="text"
+        size="40"
+        value={userInput}
+        onChange={handleChange}
+        onKeyDown={handleKeyPress}
+      />
+      <button>Add</button>
+    </form>
+  );
 }
 
-export default TodoForm
+export default TodoForm;
